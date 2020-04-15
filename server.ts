764a3ff3f34dde235d5bf4683097eca8c15570bd
Adithya Chandrasekaran <adithya.chandrasekaran@klm.com>
2020-04-15 16:09:34 +0200

    Using server db

--- server.ts
+++ server.ts
@@ -53,7 +53,7 @@ export function app() {
     }
   });
 
-  mongoose.connect('mongodb://localhost:27017/network-prediction', {
+  mongoose.connect('mongodb://adithya_c:airline1@ds163825.mlab.com:63825/heroku_bmkkf1qq', {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
